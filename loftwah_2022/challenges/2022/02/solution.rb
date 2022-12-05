d89d24bab57e957c40a6cee92962173c2504cc52
Dean Lofts <dean@deanlofts.xyz>
2022-12-05 19:37:18 +1100

    my formula was off

--- loftwah_2022/challenges/2022/02/solution.rb
+++ loftwah_2022/challenges/2022/02/solution.rb
@@ -2,40 +2,40 @@ input = File.read("input.txt")
 score = 0
 input.each_line do |line|
     if line[0] == "A" && line[2] == "Y"
-        score += 6
-        puts "Win 6 points, the current score is #{score}"
+        score += 8
+        puts "Win 8 points, the current score is #{score}"
     end
     if line[0] == "A" && line[2] == "X"
-        score += 3
-        puts "Draw 3 points, the current score is #{score}"
+        score += 1
+        puts "Draw 1 points, the current score is #{score}"
     end
     if line[0] == "A" && line[2] == "Z"
-        score += 0
-        puts "Lose 0 points, the current score is #{score}"
+        score += 3
+        puts "Lose 3 points, the current score is #{score}"
     end
     if line[0] == "B" && line[2] == "Z"
-        score += 6
-        puts "Win 6 points, the current score is #{score}"
+        score += 9
+        puts "Win 9 points, the current score is #{score}"
     end
     if line[0] == "B" && line[2] == "Y"
-        score += 3
-        puts "Draw 3 points, the current score is #{score}"
+        score += 2
+        puts "Draw 2 points, the current score is #{score}"
     end
     if line[0] == "B" && line[2] == "X"
-        score += 3
-        puts "Lose 0 points, the current score is #{score}"
+        score += 1
+        puts "Lose 1 points, the current score is #{score}"
     end
     if line[0] == "C" && line[2] == "X"
-        score += 6
-        puts "Win 6 points, the current score is #{score}"
+        score += 7
+        puts "Win 7 points, the current score is #{score}"
     end
     if line[0] == "C" && line[2] == "Z"
         score += 3
         puts "Draw 3 points, the current score is #{score}"
     end
     if line[0] == "C" && line[2] == "Y"
-        score += 0
-        puts "Lose 0 points, the current score is #{score}"
+        score += 2
+        puts "Lose 2 points, the current score is #{score}"
     end
 end
 puts "Final score: #{score}"
